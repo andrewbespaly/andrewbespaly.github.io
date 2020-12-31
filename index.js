faf497955814b6b7d63f20971a2c9d1eb9519e21
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-12-30 20:46:32 -0800

    added new project, format changes

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ function copyEmail() {
     document.execCommand('copy');
     document.body.removeChild(el);
 }
+
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.8
+  };
+  let callback = (entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.target.classList.contains('active')){
+        if(entry.isIntersecting){
+          entry.target.play();
+        } else {
+          entry.target.pause();
+        }
+      }
+    })
+  } 
+  let observer = new IntersectionObserver(callback, options);
+const allVideos = document.querySelectorAll(".active");
+allVideos.forEach((video)=>{
+    observer.observe(video);
+})
